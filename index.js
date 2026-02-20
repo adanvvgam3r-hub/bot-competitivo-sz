7f1af2c7571ed9f26cb33e8b4cd0a74c63e335e9
adanvvgam3r-hub <adanvvgam3r@gmail.com>
2026-02-20 17:53:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.GuildMembers
+    ] 
+});
+
+client.once('ready', () => {
+    console.log('✅ Bot de Competitivo Online!');
+});
+
+// Aqui você vai importar a lógica dos comandos das outras pastas
+client.login(process.env.TOKEN);
 
